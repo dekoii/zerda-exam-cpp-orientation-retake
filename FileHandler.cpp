744cb1f7e15389b5e6eec711a8e05822873f042d
dekoii <daniel.janicsak@gmail.com>
2017-01-23 12:56:35 +0100

    minor fix to filehandler

--- FileHandler.cpp
+++ FileHandler.cpp
@@ -5,7 +5,7 @@ std::string Filehandler::ReadFromFile(char filename[]) {
   if (!inputfile) {
     throw "no file provided";
   }
-  if (inputfile.fail() {
+  if (inputfile.fail()) {
     throw 'o':
   }
   std::stringstream buff;
